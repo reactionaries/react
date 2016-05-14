b72fb1889e252f2dca70ded0a727d5d50a7ca40a
Gene Troy <genetroy@hotmail.com>
2016-05-14 00:10:32 -0700

    fill out some basic utility files

--- webpack.config.js
+++ webpack.config.js
@@ -1,1 +1,19 @@
-needs more bears
+var path = require('path');
+var config = {
+  entry: path.resolve(__dirname, 'app/js/client.js'),
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: 'bundle.js'
+  },
+  module: {
+    loaders: [{
+      test: /\.jsx?$/,
+      loader: 'babel',
+      query: {
+        presets:['react']
+      }
+    }]
+  },
+};
+
+module.exports = config;
