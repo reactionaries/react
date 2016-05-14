faa093d313f6d5237b6243a7d4723b4b28ac3407
KMSkelton <kmskelton@outlook.com>
2016-05-14 14:40:27 -0700

    gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,1 +1,33 @@
-needs more bears
+const gulp = require('gulp');
+const webpack = require('webpack-stream');
+const preset = require('babel-preset-react');
+const babel = require('babel-loader');
+
+
+gulp.task('html:dev', () => {
+  gulp.src(__dirname + '/app/**/*.html')
+    .pipe(gulp.dest(__dirname + '/build'));
+});
+
+gulp.task('webpack:dev', () => {
+gulp.src(__dirname + '/app/js/client.js')
+  .pipe(webpack({
+    output: {
+      filename: 'bundle.js'
+    },
+    module: {
+      loaders: [
+        {
+          test: /\.js$/,
+          exclude: /(node_modules)/,
+          loaders: [ 'babel-loader?presets[]=react' ]
+        }
+      ]
+    }
+  }))
+  .pipe(gulp.dest('build/'));
+});
+
+gulp.task('build:dev', ['webpack:dev', 'html:dev']);
+
+gulp.task('default', 'build:dev');
