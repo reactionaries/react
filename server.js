a2dd048ede2c90e14d09867a12df455c70a38305
Gene Troy <genetroy@hotmail.com>
2016-05-14 14:31:54 -0700

    pair programming, new server

--- server.js
+++ server.js
@@ -1,1 +1,21 @@
-needs more bears
+const express = require('express');
+const app = module.exports = exports = express();
+const mongoose = require('mongoose');
+const PORT = process.env.PORT || 3000;
+mongoose.connect(process.env.MONGO_URI || 'mongodb://localhost/bearChatDb');
+
+const bearChatRouter = require(__dirname + '/lib/bear_routes_3000');
+
+app.use(
+  express.static(__dirname + '/build')
+);
+
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Origin, X-Requested-With, Accept');
+  res.header('Access-Control-Allow-Methods', 'POST, GET, PUT, DELETE, PATCH, OPTIONS');
+  next();
+});
+
+app.use('/api', bearChatRouter);
+app.listen(PORT, () => console.log('bears listening on port ' + PORT));
